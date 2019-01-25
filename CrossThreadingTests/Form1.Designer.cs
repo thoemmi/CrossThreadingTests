63c2d0c7926cdd2de02a93d60e6a4c73cbcd5967
Thomas Freudenberg <info@thomasfreudenberg.com>
2019-01-25 14:49:06 +0100

    Add project files.

---
+++ CrossThreadingTests/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace CrossThreadingTests
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 41);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click Me 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(24, 137);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(748, 287);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(238, 41);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Click Me 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(489, 41);
+            this.button3.Margin = new System.Windows.Forms.Padding(6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 44);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Click Me 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CrossThreadingDemo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ CrossThreadingTests/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.Threading;
+
+namespace CrossThreadingTests
+{
+    public partial class Form1 : Form
+    {
+        private readonly JoinableTaskFactory _joinableTaskFactory;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _joinableTaskFactory  = new JoinableTaskFactory(new JoinableTaskContext());
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            // await a completed task => will continue synchronously
+            await SomeFastAsyncOperation().ConfigureAwait(false);
+
+            // await a slow task => will continue in another thread
+            var t = await SomeSlowAsyncOperation().ConfigureAwait(false);
+
+            // write text to text box
+            textBox1.Text = t;
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            // await a completed task => will continue synchronously
+            await SomeFastAsyncOperation().ConfigureAwait(false);
+
+            // await a slow task => will continue in another thread
+            var t = SomeSlowAsyncOperation().ConfigureAwait(false).GetAwaiter().GetResult();
+
+            // write text to text box
+            textBox1.Text = t;
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            // force switch to threadpool thread
+            await TaskScheduler.Default;
+
+            // await a completed task => will continue synchronously
+            await SomeFastAsyncOperation().ConfigureAwait(false);
+
+            // await a slow task => will continue in another thread
+            var t = await SomeSlowAsyncOperation().ConfigureAwait(false);
+
+            // switch to main thread
+            await _joinableTaskFactory.SwitchToMainThreadAsync();
+
+            // write text to text box
+            textBox1.Text = t;
+        }
+
+        private static Task<string> SomeFastAsyncOperation()
+        {
+            return Task.FromResult("test");
+        }
+
+        private async Task<string> SomeSlowAsyncOperation()
+        {
+            var httpClient = new HttpClient();
+            var result = await httpClient.GetStringAsync("http://microsoft.com");
+            return result;
+        }
+    }
+}
